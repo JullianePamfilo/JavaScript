ecf9531b6e5a8d28270bd4371a9cfcd0bc3b29ce
Julliane Pamfilo <pamfilojulliane@yahoo.com>
2023-02-03 22:25:47 -0600

    JavaScript

---
+++ script.js
@@ -0,0 +1,17 @@
+// console.log("hello world!")
+
+function random() {
+    alert("h1")
+}
+
+// Phil.innerText = "Steven"
+
+function sayIt(e) {
+    e.preventDefault()
+    console.log(spokenText)
+    spoken.innerText= spokenText.value
+}
+
+function turnBlue(el) {
+    el.style.backgroundColor = 'blue'
+}
